5bac970cf44764d7a6834a9e6a124efe655150de
Josh-Decime <josh_decime@yahoo.com>
2023-12-03 23:58:14 -0700

    Finished!

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ let clickUpgrades = [
         quantity: 0,
         multiplier: 1,
         unlocked: false,
-        // quantityStatID: 'toolQuantity',
-        // details: 'hammer',
-        // nameTwo: 'please',
+
     },
     {
         name: 'Workbench',
@@ -41,7 +39,7 @@ let automaticUpgrades = [
 let presents = 0
 let clickPower = 1
 let autoPower = 0
-// let upgradeNewPrice = 0
+
 
 
 function mine() {
@@ -67,27 +65,7 @@ function drawAutoPower() {
     autoElm.innerHTML = autoPower.toString()
 }
 
-// function drawToolQuantity() {
-//     let toolElm = document.getElementById('toolQuantity')
-//     toolElm.innerHTML = clickUpgrades.quantity
-// }
 
-
-// would update all prices, i need a for each loop
-// function updateUpgradePrice() {
-//     let upgradeElm = document.getElementById('Upgrade Price')
-//     upgradeElm.innerHTML = upgradeNewPrice.toString()
-// }
-
-// from our examples I don't see any way to update the price of each upgrade with one function.. but I do know how to do it individually, so I will just do that then
-// function updateUpgradesPrice() {
-//     clickUpgrades.forEach(upgrade => {
-//         let updateElm = document.getElementById(upgrade.price)
-//         console.log('update elm', updateElm)
-//     })
-// }
-
-// lol i was way over complicating it... i need to FIND the upgrade and display it
 function drawClickUpgradePrice(upgradeName) {
 
     let clickUpgrade = clickUpgrades.find(upgrade => upgrade.name == upgradeName)
@@ -95,14 +73,9 @@ function drawClickUpgradePrice(upgradeName) {
     console.log('price', clickUpgrade.price)
     let upgradeElm = document.getElementById(upgradeName)
     upgradeElm.innerHTML = clickUpgrade.price.toString()
-    // uuugh why are you saying cant set property of null??? It literally is console logging price in the line right above it!! 
-    // figured it out, there was an issue with the HTML. when i was calling it I had '' around Tools.. so it said 'Tools'
 }
 
-// function drawClickQuantity(UpgradeId) {
-//     let clickUpgrade = clickUpgrades.find(Upgrade => Upgrade.Details == UpgradeId)
-//     console.log('find ID', clickUpgrade)
-// }
+
 
 function drawAutoUpgradePrice(upgradeName) {
     let autoUpgrade = automaticUpgrades.find(upgrade => upgrade.name == upgradeName)
@@ -112,14 +85,7 @@ function drawAutoUpgradePrice(upgradeName) {
     upgradeElm.innerHTML = autoUpgrade.price.toString()
 }
 
-// doesnt work & I am not sure why.. i checked for typos in function, array & HTML. even changed names & updated them all a couple times
-// function drawClickStatQuantity(quantityStatID) {
-//     let clickUpgradeStat = clickUpgrades.find(upgrade => upgrade.statQuantityID == quantityStatID)
-//     console.log('finding upgrades stat', clickUpgradeStat)
-//     console.log('quantity', clickUpgradeStat.quantity)
-//     let upgradeElm = document.getElementById(quantityStatID)
-//     upgradeElm.innerHTML = clickUpgradeStat.quantity.toString()
-// }
+
 
 function drawToolQuantity() {
     let toolQuantityElm = document.getElementById('toolQuantity')
@@ -163,6 +129,7 @@ function drawPapaElfMultiplier() {
 }
 
 
+
 function buyClickUpgrade(upgradeName) {
     let upgrade = clickUpgrades.find(item => item.name == upgradeName)
     console.log(upgradeName, '|', upgrade)
@@ -172,20 +139,18 @@ function buyClickUpgrade(upgradeName) {
         console.log('new presents count:', presents)
         upgrade.quantity++
         console.log('power-up count:', upgrade.quantity)
-        // console.log(upgrade, 'upgrade we are purchasing')
+        console.log(upgrade, 'upgrade we are purchasing')
         clickPower += upgrade.multiplier
-        // console.log(clickPower, 'clickPower')
+        console.log(clickPower, 'clickPower')
         upgrade.price = Math.round(upgrade.price * 1.5)
-        // ANCHOR this is an attempt to update upgrade cost, it can probably be commented out
-        // upgradeNewPrice = upgrade.price
         console.log('upgrade price:', upgrade)
 
         updatePresents()
         drawClickPower()
         drawClickUpgradePrice(upgradeName)
-        // updateUpgradePrice()
 
-        // drawClickStatQuantity(upgradeName)
+
+
         drawToolQuantity()
         drawWorkshopQuantity()
         drawToolMultiplier()
@@ -223,7 +188,7 @@ function automaticIncrease() {
         if (upgrade.quantity > 0) {
             let autoGains = upgrade.quantity * upgrade.multiplier
             presents += autoGains
-            // console.log('upgrade', upgrade, '|', 'autoGains', autoGains, '|', 'presents', presents)
+            console.log('upgrade', upgrade, '|', 'autoGains', autoGains, '|', 'presents', presents)
 
             updatePresents()
         }
@@ -236,7 +201,6 @@ let autoInterval = setInterval(automaticIncrease, 1000)
 // test timer
 // let countdownTime = 1 * 10 * 1000
 
-// ANCHOR this is the actual countdown timer
 let countdownTime = 12 * 60 * 1000
 
 function updateTimer() {
@@ -247,7 +211,6 @@ function updateTimer() {
 
     document.getElementById('timer').innerHTML = `${minutes}m ${seconds}s`
 
-    // ANCHOR haven't figured out how to make this work yet
     if (countdownTime <= 0 && presents < 100000) {
         alert('Christmas is Ruined!')
     }
@@ -261,32 +224,3 @@ setInterval(updateTimer, 1000)
 
 
 updateTimer()
-
-
-
-// function clickValue() {
-// we want to increase clickPower by the multiplier * quantity forEach clickUpgrades
-
-// clickUpgrades.forEach(upgrade => {
-//     console.log(upgrade)
-// })
-
-// i think this is Sam's code
-// clickUpgrades.forEach(upgrade => {
-//     console.log('upgrade', upgrade)
-//     let upgradePower = upgrade.multiplier * upgrade.quantity
-//     console.log(upgradePower, 'power')
-//     clickPower += upgradePower
-// })
-
-// clickPower += (upgradeAmount * upgradePower)
-// console.log('click Power:', clickPower)
-// }
-
-// or maybe this was hers? i wrote one of these.. so wow.. i basically had it..
-// clickUpgrades.forEach(upgrade => {
-//     if (upgrade.quantity > 0) {
-//         let upgradePower = upgrade.multiplier * upgrade.quantity
-//         presents += upgradePower
-//     }
-// })
